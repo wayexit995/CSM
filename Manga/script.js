426110c7b4d4d20162dfafcfef1c11887426f92c
wayexit995 <105863400+wayexit995@users.noreply.github.com>
2024-11-04 09:52:55 +0530

    Update script.js

--- Manga/script.js
+++ Manga/script.js
@@ -2,7 +2,4 @@ const volumeLinks = document.querySelectorAll('.volume-list a');
 
 volumeLinks.forEach(function(link) {
 	link.addEventListener('click', function(event) {
-		event.preventDefault(); // Corrected with parentheses
-		// You can add any other actions you want to perform on click here
-	});
-});
+		event.preventDefault 
